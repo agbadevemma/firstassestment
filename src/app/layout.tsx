ec95ee81705cc9e121c85041cf67f2dff89471d7
himarnoel <olaimarnoel@gmail.com>
2026-05-19 22:07:58 +0100

    feat: update layout and home page with Toaster and QueryClientProvider integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Toaster } from "sonner";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/query-provider";
+
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -30,7 +34,9 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+     
+        <Toaster />
+      <body className="min-h-full flex flex-col">  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider></body>
     </html>
   );
 }
